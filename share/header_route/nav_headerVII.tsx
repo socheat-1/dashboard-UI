71c69ab0f8612f32065b18b7641421738dda01c4
Socheat <kinsocheat880@gmail.com>
2025-09-17 17:03:53 +0700

    new update

--- share/header_route/nav_headerVII.tsx
+++ share/header_route/nav_headerVII.tsx
@@ -9,12 +9,12 @@ export default function NavHeaderVII({ title = "", home = "", label = "", href =
 
     return (
         <div className="">
-            <h1 className="text-[24px] font-[500] rounded-[3px] text-indigo-600 dark:text-gray-200 mb-2">
+            <h1 className="text-[20px] font-[500] rounded-[3px] text-indigo-600 dark:text-gray-200 mb-2">
                 <span>{title}</span>
             </h1>
             <div className="flex items-center">
                 {items.map((item, index) => (
-                    <div key={index} className="flex justify-center text-indigo-500 dark:text-gray-200 items-center space-x-2 text-[16px] font-light">
+                    <div key={index} className="flex justify-center text-indigo-500 dark:text-gray-200 items-center space-x-2 text-[15px] font-light">
                         <h1>{item.home}</h1>
                         <p><MdKeyboardArrowRight /></p>
                         <Link href={item.href} className="hover:underline">
